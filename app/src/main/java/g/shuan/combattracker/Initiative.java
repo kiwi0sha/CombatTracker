cbc3c81a1ef56f108abfb5b2cad662c8e5a3199d
shuan <Shaun.tom92@gmail.com>
2019-05-20 18:37:25 +1200

    started initiative input for turn order

---
+++ app/src/main/java/g/shuan/combattracker/Initiative.java
@@ -0,0 +1,13 @@
+package g.shuan.combattracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Initiative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initiative);
+    }
+}
